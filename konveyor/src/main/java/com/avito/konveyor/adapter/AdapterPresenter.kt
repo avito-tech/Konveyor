541c5318dd9c77ff2c290abad47cbd1c904f1f03
Philip Uvarov <philuvarov@googlemail.com>
2017-08-20 16:58:26 +0200

    Public API Documentation
    Add documentation for public API

--- konveyor/src/main/java/com/avito/konveyor/adapter/AdapterPresenter.kt
+++ konveyor/src/main/java/com/avito/konveyor/adapter/AdapterPresenter.kt
@@ -1,20 +1,41 @@
 package com.avito.konveyor.adapter
 
+import android.support.v7.widget.RecyclerView
 import com.avito.konveyor.blueprint.Item
 import com.avito.konveyor.blueprint.ItemView
 import com.avito.konveyor.data_source.DataSource
 
-
+/**
+ * Base abstraction over [RecyclerView.Adapter]
+ */
 interface AdapterPresenter {
 
+    /**
+     * Will be called on [RecyclerView.Adapter.onBindViewHolder]
+     */
     fun bindView(view: ItemView, position: Int)
 
+    /**
+     * Will be called on [RecyclerView.Adapter.getItemCount]
+     */
     fun getCount(): Int
 
+    /**
+     * Will be called on [RecyclerView.Adapter.getItemViewType]
+     */
     fun getViewType(position: Int): Int
 
+    /**
+     * Will be called on [RecyclerView.Adapter.getItemId]
+     */
     fun getItemId(position: Int): Long
 
+    /**
+     * Method to notify [AdapterPresenter] that data has been changed
+     *
+     * <em>Note:</em> if you are using [SimpleRecyclerAdapter] you still need to call
+     * [RecyclerView.Adapter.notifyDataSetChanged] in order to notify your adapter about changes
+     */
     fun onDataSourceChanged(dataSource: DataSource<out Item>)
 
     fun isEmpty() = getCount() == 0
--- konveyor/src/main/java/com/avito/konveyor/blueprint/ItemBlueprint.kt
+++ konveyor/src/main/java/com/avito/konveyor/blueprint/ItemBlueprint.kt
@@ -1,14 +1,35 @@
 package com.avito.konveyor.blueprint
 
+import android.support.v7.widget.RecyclerView
 import com.avito.konveyor.adapter.BaseViewHolder
 import com.avito.konveyor.blueprint.ViewHolderBuilder.ViewHolderProvider
+import com.avito.konveyor.exception.ViewTypeCollisionException
 
+/**
+ * Key entity that ties together Android world with a set of abstraction provided by Konveyor
+ */
 interface ItemBlueprint<in V : ItemView, in I : Item> {
 
+    /**
+     * Presenter associated with this view and data
+     */
     val presenter: ItemPresenter<V, I>
 
+    /**
+     * Creates associated [RecyclerView.ViewHolder]
+     *
+     * @see ViewHolderBuilder
+     */
     val viewHolderProvider: ViewHolderProvider<BaseViewHolder>
 
+    /**
+     * This method need to be implemented in order to build connection between given [Item]
+     * and its [ItemBlueprint].
+     *
+     * Please mind, in the list of registered [ItemBlueprint]s every result of this function
+     * has to be unique. If multiple registered [ItemBlueprint]s respond `true` to a given [Item],
+     * [ViewTypeCollisionException] will be thrown.
+     */
     fun isRelevantItem(item: Item): Boolean
 
 }
