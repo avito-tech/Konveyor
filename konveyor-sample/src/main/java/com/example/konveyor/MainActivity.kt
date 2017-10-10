e40fda8a98f92561850a3b8201ce2d9fbddb0cb9
Mike Casper <Mikecasper395@gmail.com>
2017-10-09 19:02:58 -0500

    Address PR comments

--- konveyor-sample/src/main/java/com/example/konveyor/MainActivity.kt
+++ konveyor-sample/src/main/java/com/example/konveyor/MainActivity.kt
@@ -7,16 +7,28 @@ import android.support.v7.widget.RecyclerView
 import com.avito.konveyor.ItemBinder
 import com.avito.konveyor.adapter.SimpleAdapterPresenter
 import com.avito.konveyor.adapter.SimpleRecyclerAdapter
+import com.avito.konveyor.data_source.ListDataSource
 import com.example.konveyor.items.VerticalSpaceItemDecoration
+import com.example.konveyor.items.article.Article
 import com.example.konveyor.items.article.ArticleBlueprint
 import com.example.konveyor.items.article.ArticlePresenter
+import com.example.konveyor.items.photo.Photo
 import com.example.konveyor.items.photo.PhotoBlueprint
 import com.example.konveyor.items.photo.PhotoPresenter
 
 class MainActivity : AppCompatActivity() {
 
     private val itemList: RecyclerView by lazy { findViewById(R.id.itemList) as RecyclerView }
-    private val dataSource = LocalDataSource()
+    private val dataSource = ListDataSource(listOf(
+            Article(1, "Northern Lights", R.string.article_body_preview, R.drawable.article1),
+            Photo(1, "Full Moon", R.drawable.photo1),
+            Photo(2, "The Starry Sky", R.drawable.photo2),
+            Article(2, "An Ancient Discovery", R.string.article_body_preview, R.drawable.article2),
+            Photo(3, "Racing Through the Sand", R.drawable.photo3),
+            Article(3, "An Increase in Nightlife", R.string.article_body_preview, R.drawable.article3),
+            Article(4, "The Effects of a Day at the Beach", R.string.article_body_preview, R.drawable.article4),
+            Photo(4, "A Normal Day", R.drawable.photo4)
+    ))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
         val presenter = SimpleAdapterPresenter(binder, binder)
-        itemList.adapter = SimpleRecyclerAdapter(presenter, binder)
+        itemList.adapter = SimpleRecyclerAdapter(presenter, binder).apply { setHasStableIds(true) }
 
         presenter.onDataSourceChanged(dataSource)
         itemList.adapter.notifyDataSetChanged()
--- konveyor-sample/src/main/java/com/example/konveyor/items/article/Article.kt
+++ konveyor-sample/src/main/java/com/example/konveyor/items/article/Article.kt
@@ -4,4 +4,4 @@ import android.support.annotation.DrawableRes
 import android.support.annotation.StringRes
 import com.avito.konveyor.blueprint.Item
 
-data class Article(override val id: Long, val title: String, @StringRes val bodyPreview: Int, @DrawableRes val image: Int) : Item
+class Article(override val id: Long, val title: String, @StringRes val bodyPreview: Int, @DrawableRes val image: Int) : Item
